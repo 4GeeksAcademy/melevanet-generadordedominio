e4bdf4d07570e944d346506eb531f425a4df5275
melevanet <157515892+melevanet@users.noreply.github.com>
2024-03-27 20:05:31 +0000

    melevanet-generadordedominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = [".com", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < com.length; z++) {
+          console.log(pronoun[i] + adj[x] + noun[y] + com[z]);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
